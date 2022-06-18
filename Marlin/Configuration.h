2c486cc6dda01b7cf2f7a9001d39200559104b28
martin1951 <66937958+martin1951@users.noreply.github.com>
2022-06-18 16:40:45 -0700

    Symmetric probing configuration
    Updated configuration files for symmetric probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1282,7 +1282,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 10
+//#define PROBING_MARGIN 10
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_FEEDRATE (150*60)
@@ -1494,9 +1494,9 @@
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define Y_MIN_POS -9
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
+#define X_MAX_POS X_BED_SIZE + 15
 #define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
